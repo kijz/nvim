87b7e99606d3fefe8c88123020ef70b86021a00d
kijz <filipkajzer@gmail.com>
2024-06-07 15:29:52 +0200

    fix: treesitter textobjects, improve mapping also

--- lua/custom/configs/overrides.lua
+++ lua/custom/configs/overrides.lua
@@ -41,15 +41,36 @@ M.treesitter = {
   textobjects = {
     select = {
       enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+      lookahead = true, -- automatically jump forward to textobj, similar to targets.vim
       keymaps = {
         -- You can use the capture groups defined in textobjects.scm
-        ["aa"] = "@parameter.outer",
-        ["ia"] = "@parameter.inner",
-        ["af"] = "@function.outer",
-        ["if"] = "@function.inner",
-        ["ac"] = "@class.outer",
-        ["ic"] = "@class.inner",
+        ["a="] = { query = "@assignment.outer", desc = "Select outer part of an assignment" },
+        ["i="] = { query = "@assignment.inner", desc = "Select inner part of an assignment" },
+        ["l="] = { query = "@assignment.lhs", desc = "Select left hand side of an assignment" },
+        ["r="] = { query = "@assignment.rhs", desc = "Select right hand side of an assignment" },
+
+        ["a:"] = { query = "@property.outer", desc = "Select outer part of an object property" },
+        ["i:"] = { query = "@property.inner", desc = "Select inner part of an object property" },
+        ["l:"] = { query = "@property.lhs", desc = "Select left part of an object property" },
+        ["r:"] = { query = "@property.rhs", desc = "Select right part of an object property" },
+
+        ["aa"] = { query = "@parameter.outer", desc = "Select outer part of a parameter/argument" },
+        ["ia"] = { query = "@parameter.inner", desc = "Select inner part of a parameter/argument" },
+
+        ["ai"] = { query = "@conditional.outer", desc = "Select outer part of a conditional" },
+        ["ii"] = { query = "@conditional.inner", desc = "Select inner part of a conditional" },
+
+        ["al"] = { query = "@loop.outer", desc = "Select outer part of a loop" },
+        ["il"] = { query = "@loop.inner", desc = "Select inner part of a loop" },
+
+        ["af"] = { query = "@call.outer", desc = "Select outer part of a function call" },
+        ["if"] = { query = "@call.inner", desc = "Select inner part of a function call" },
+
+        ["am"] = { query = "@function.outer", desc = "Select outer part of a method/function definition" },
+        ["im"] = { query = "@function.inner", desc = "Select inner part of a method/function definition" },
+
+        ["ac"] = { query = "@class.outer", desc = "Select outer part of a class" },
+        ["ic"] = { query = "@class.inner", desc = "Select inner part of a class" },
       },
     },
     move = {
@@ -60,7 +81,7 @@ M.treesitter = {
         ["]]"] = "@class.outer",
       },
       goto_next_end = {
-        ["]M"] = "@function.outer",
+        ["]m"] = "@function.outer",
         ["]["] = "@class.outer",
       },
       goto_previous_start = {
@@ -68,7 +89,7 @@ M.treesitter = {
         ["[["] = "@class.outer",
       },
       goto_previous_end = {
-        ["[M"] = "@function.outer",
+        ["[m"] = "@function.outer",
         ["[]"] = "@class.outer",
       },
     },
--- lua/custom/plugins.lua
+++ lua/custom/plugins.lua
@@ -117,6 +117,9 @@ local plugins = {
 
   {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     opts = overrides.treesitter,
   },
 
