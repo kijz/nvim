7417fea3697f073ccdfee13abd4b35f62ffab559
kijz <filipkajzer@gmail.com>
2026-01-14 11:55:51 +0100

    chore: migrate 2.0 cfg to 2.5

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,10 +1,109 @@
 require "nvchad.mappings"
 
--- add yours here
-
 local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- screenshots
+map("v", "<leader>sc", ":Silicon<CR>", { desc = "[s]creenshot [c]ode"})
+
+-- Tabufline
+map("n", "<leader>xa", function()
+  require("nvchad.tabufline").closeAllBufs(true)
+end, { desc = "Close all buffers" })
+
+map("n", "<leader>xo", function()
+  require("nvchad.tabufline").closeAllBufs(false)
+end, { desc = "Close all buffers except current" })
+
+-- DAP
+map("n", "<leader>tb", function()
+  require("dap").toggle_breakpoint()
+end, { desc = "[t]oggle [b]reakpoint" })
+
+map("n", "<leader>dc", function()
+  require("dap").continue()
+end, { desc = "[d]ebug [c]ontinue" })
+
+map("n", "<leader>dt", function()
+  require("dap").terminate()
+end, { desc = "[d]ap [t]erminate" })
+
+map("n", "<leader>so", function()
+  require("dap").step_over()
+end, { desc = "[s]tep [o]ver code" })
+
+map("n", "<leader>si", function()
+  require("dap").step_into()
+end, { desc = "[s]tep [i]nto code" })
+
+map("n", "<leader>dr", function()
+  require("dap").repl.open()
+end, { desc = "Inspect state via DAP REPL" })
+
+-- Gitsigns
+map("n", "<leader>gq", function()
+  require("gitsigns").setloclist()
+end, { desc = "Hunk locations" })
+
+map("n", "<leader>gh", function()
+  require("gitsigns").setloclist()
+end, { desc = "Hunk locations" })
+
+-- LazyGit
+map("n", "<leader>gg", "<cmd>LazyGit<CR>", { desc = "Open :LazyGit" })
+
+-- Telescope (LSP + Git + Diagnostics + Symbols + Grep)
+map("n", "gr", function()
+  require("telescope.builtin").lsp_references()
+end, { desc = "[g]o [r]eference" })
+
+map("n", "gd", function()
+  require("telescope.builtin").lsp_definitions()
+end, { desc = "[g]o [d]efinition" })
+
+map("n", "gi", function()
+  require("telescope.builtin").lsp_implementations()
+end, { desc = "[g]o [i]mplementation" })
+
+map("n", "gt", function()
+  require("telescope.builtin").lsp_type_definitions()
+end, { desc = "[g]o [t]ype definition" })
+
+map("n", "<leader>q", function()
+  require("telescope.builtin").diagnostics({ bufnr = 0 })
+end, { desc = "Search buffer diagnostics" })
+
+map("n", "<leader>qq", function()
+  require("telescope.builtin").diagnostics()
+end, { desc = "Search diagnostics" })
+
+map("n", "<leader>gs", function()
+  require("telescope.builtin").git_status()
+end, { desc = "[g]it [s]tatus" })
+
+map("n", "<leader>gc", function()
+  require("telescope.builtin").git_bcommits()
+end, { desc = "[g]it buffer [c]ommit" })
+
+map("n", "<leader>gf", function()
+  require("telescope.builtin").git_files()
+end, { desc = "[g]it [f]iles" })
+
+map("n", "<leader>fs", function()
+  require("telescope.builtin").lsp_document_symbols()
+end, { desc = "[f]ind [s]ymbols" })
+
+map("n", "<leader>ws", function()
+  require("telescope.builtin").lsp_dynamic_workspace_symbols()
+end, { desc = "[w]orkspace [s]ymbols" })
+
+map("n", "fw", function()
+  require("telescope.builtin").grep_string()
+end, { desc = "[f]ind [W]ord under cursor" })
+
+map("v", "<leader>gc", function()
+  require("telescope.builtin").git_bcommits_range()
+end, { desc = "[g]it buffer [c]ommit range" })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,25 +4,134 @@ return {
     -- event = 'BufWritePre', -- uncomment for format on save
     opts = require "configs.conform",
   },
-
-  -- These are some examples, uncomment them if you want to see them work!
   {
     "neovim/nvim-lspconfig",
     config = function()
       require "configs.lspconfig"
     end,
   },
-
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+  { import = "nvchad.blink.lazyspec" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim", "lua", "vimdoc",
+        "html", "css"
+      },
+    },
+  },
+  {
+    "yetone/avante.nvim",
+    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    -- ⚠️ must add this setting! ! !
+    build = vim.fn.has("win32") ~= 0
+        and "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false"
+        or "make",
+    event = "VeryLazy",
+    version = false, -- Never set this value to "*"! Never!
+    ---@module 'avante'
+    ---@type avante.Config
+    opts = {
+      -- add any opts here
+      -- this file can contain specific instructions for your project
+      instructions_file = "avante.md",
+      -- for example
+      provider = "grok",
+      providers = {
+        grok = {
+          __inherited_from = 'openai',
+          endpoint = 'https://openrouter.ai/api/v1',
+          api_key_name = 'OPENROUTER_API_KEY',
+          model = 'x-ai/grok-code-fast-1:online',
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 20480,
+          },
+        },
+        claude = {
+          endpoint = "https://api.anthropic.com",
+          model = "claude-sonnet-4-20250514",
+          timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 20480,
+          },
+        },
+        moonshot = {
+          endpoint = "https://api.moonshot.ai/v1",
+          model = "kimi-k2-0711-preview",
+          timeout = 30000, -- Timeout in milliseconds
+          extra_request_body = {
+            temperature = 0.75,
+            max_tokens = 32768,
+          },
+        },
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick",         -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+      "stevearc/dressing.nvim",        -- for input provider dressing
+      "folke/snacks.nvim",             -- for input provider snacks
+      "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+      "zbirenbaum/copilot.lua",        -- for providers='copilot'
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+  {
+    "michaelrommel/nvim-silicon",
+    lazy = true,
+    cmd = "Silicon",
+    config = function()
+      require("silicon").setup({
+        font = "JetBrainsMono Nerd Font=34;Noto Color Emoji=34",
+        theme = "Dracula",
+        background = "#e1e1e1",
+      })
+    end
+  },
+  {
+    "kdheepak/lazygit.nvim",
+    lazy = true,
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+  },
 }
