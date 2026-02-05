7d9beecda5969100c66f75650d873366a32e32d9
kijz <filipkajzer@gmail.com>
2026-02-05 16:19:56 +0100

    feat: cmp up/down keys

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -8,3 +8,13 @@ autocmd("TextYankPost", {
     vim.highlight.on_yank { higroup = "IncSearch", timeout = 200 }
   end,
 })
+
+vim.schedule(function()
+  local cmp_ok, cmp = pcall(require, "cmp")
+  if cmp_ok then
+    local current_config = cmp.get_config()
+    current_config.mapping["<Down>"] = cmp.mapping.select_next_item()
+    current_config.mapping["<Up>"] = cmp.mapping.select_prev_item()
+    cmp.setup(current_config)
+  end
+end)
